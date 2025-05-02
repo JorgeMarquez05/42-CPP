039fb135f9fd6d87ddf92fd388a6c67b1addc84e
Jorge Marquez <jorgemiliano91@gmail.com>
2025-05-03 00:22:20 +0100

    Build: CPP03 ex01

---
+++ CPP-03/ex01/main.cpp
@@ -0,0 +1,23 @@
+#include "ScavTrap.hpp"
+
+int	main(void)
+{
+	ScavTrap bot("Trapii");
+	bot.attack("Enemy");
+	bot.attack("Enemy");
+	bot.attack("Enemy");
+	bot.attack("Enemy");
+	bot.attack("Enemy");
+	bot.attack("Enemy");
+	bot.attack("Enemy");
+	bot.attack("Enemy");
+	bot.attack("Enemy");
+	// bot.attack("Enemy");
+	// bot.attack("Enemy");
+	bot.guardGate();
+	bot.takeDamage(500);
+	// bot.takeDamage(5);
+	bot.beRepaired(3);
+	bot.takeDamage(5);
+	bot.beRepaired(3);
+}

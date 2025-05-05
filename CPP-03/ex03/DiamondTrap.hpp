52bcf037fa59567fee8debbc0665533dfacbc0b5
Jorge  Marquez <joralves@student.42lisboa.com>
2025-05-05 14:38:22 +0100

    Working with multiple inherence

--- CPP-03/ex03/DiamondTrap.hpp
+++ CPP-03/ex03/DiamondTrap.hpp
@@ -8,11 +8,13 @@ class DiamondTrap : public ScavTrap, public FragTrap
 private:
     std::string name;
 public:
+    DiamondTrap();
     DiamondTrap(std::string name);
     ~DiamondTrap();
 };
 
-DiamondTrap::DiamondTrap(std::string n)
+DiamondTrap::DiamondTrap():ScavTrap(),FragTrap(){}
+DiamondTrap::DiamondTrap(std::string n): ScavTrap(n), FragTrap(n),  name(n+"clap_name")
 {
 }
 
--- CPP-03/ex03/main.cpp
+++ CPP-03/ex03/main.cpp
@@ -1,26 +1,28 @@
 #include "ClapTrap.hpp"
-#include "FragTrap.hpp"
-#include "ScavTrap.hpp"
+// #include "FragTrap.hpp"
+// #include "ScavTrap.hpp"
+#include "DiamondTrap.hpp"
 
 int	main(void)
 {
-	FragTrap bot("bot");
-	bot.attack("Enemy");
-	bot.attack("Enemy");
-	bot.attack("Enemy");
-	bot.attack("Enemy");
-	bot.attack("Enemy");
-	bot.attack("Enemy");
-	bot.attack("Enemy");
-	bot.attack("Enemy");
-	bot.attack("Enemy");
+	// DiamondTrap bot;
+	DiamondTrap bot("bot");
 	// bot.attack("Enemy");
 	// bot.attack("Enemy");
-	// bot.guardGate();
-	bot.highFivesGuys();
+	// bot.attack("Enemy");
+	// bot.attack("Enemy");
+	// bot.attack("Enemy");
+	// bot.attack("Enemy");
+	// bot.attack("Enemy");
+	// bot.attack("Enemy");
+	// bot.attack("Enemy");
+	// // bot.attack("Enemy");
+	// // bot.attack("Enemy");
+	// // bot.guardGate();
+	// bot.highFivesGuys();
 	bot.takeDamage(500);
+	// // bot.takeDamage(5);
+	// bot.beRepaired(3);
 	// bot.takeDamage(5);
-	bot.beRepaired(3);
-	bot.takeDamage(5);
-	bot.beRepaired(3);
+	// bot.beRepaired(3);
 }

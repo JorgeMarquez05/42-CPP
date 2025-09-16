ee3c8d7d9fca8a0690039ccbbb795616cc34e639
Jorge  Marquez <joralves@student.42lisboa.com>
2025-09-16 17:07:29 +0100

    Finish CPP-05 ex00

--- CPP-05/ex00/Bureaucrat.cpp
+++ CPP-05/ex00/Bureaucrat.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
+#include "Bureaucrat.hpp"
 
-class Bureacrat
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(150)
 {
-private:
-    std::string _name;
-    int _grade;
-public:
-    Bureacrat();
-    Bureacrat(std::string name);
-    std::string getName();
-    int getGrade();
-    ~Bureacrat();
-};
-
-Bureacrat::Bureacrat() 
-{
-    _name = "Deafult";
-    std::cout << "Bureacrat Default Constructor Called" << std::endl;
+    std::cout << "Bureaucrat Default Constructor Called" << std::endl;
 }
-Bureacrat::Bureacrat(std::string name) : _name(name)
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name), _grade(grade)
 {
-    std::cout << "Bureacrat Constructor Called" << std::endl;
+    if (_grade < 1)
+        throw Bureaucrat::GradeTooHighException("Too High");
+    else if (_grade > 150)
+        throw Bureaucrat::GradeTooLowException("Too Low");
+    std::cout << "Bureaucrat Constructor Called" << std::endl;
 }
 
-std::string Bureacrat::getName()
+std::string Bureaucrat::getName() const
 {
-
+    return (_name);
 }
-int Bureacrat::getGrade()
+int Bureaucrat::getGrade() const
 {
-
+    return (_grade);
 }
 
-Bureacrat::~Bureacrat()
+void Bureaucrat::increaseGrade() {
+    _grade = _grade - 1;
+    if(_grade < 1)
+        throw Bureaucrat::GradeTooHighException("Too High");
+}
+void Bureaucrat::decreaseGrade() {
+    _grade = _grade + 1;
+    if(_grade > 150)
+        throw Bureaucrat::GradeTooLowException("Too Low");
+}
+
+Bureaucrat::~Bureaucrat()
 {
 }
+std::ostream &operator<<(std::ostream &os, Bureaucrat &object)
+{
+    os << object.getName() << ", bureaucrat grade " << object.getGrade() << ".";
+    return (os);
+}
+
+// Bureaucrat::GradeTooHighException or a Bureaucrat::GradeTooLowException
--- CPP-05/ex00/main.cpp
+++ CPP-05/ex00/main.cpp
@@ -1,8 +1,20 @@
 #include <cstring>
 #include <iostream>
+#include "Bureaucrat.hpp"
 
 int main()
 {
-	std::cout << "Main Bureacrat " << std::endl;
+    try
+    {
+       Bureaucrat a("Alisson", 150);
+       std::cout << a << std::endl;
+       a.decreaseGrade();
+       std::cout << a << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what();
+    }
+
 	return (0);
 }

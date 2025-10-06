9a69584d4964b2afca566facaad470728b75a66e
Jorge  Marquez <joralves@student.42lisboa.com>
2025-10-06 11:19:21 +0100

    Fix: Correcting the ex01

--- CPP-05/ex01/main.cpp
+++ CPP-05/ex01/main.cpp
@@ -3,23 +3,78 @@
 #include <cstring>
 #include <iostream>
 
+
+static void testConstructor()
+{
+	std::cout << "\n*********TESTING CONSTRUCTOR EXCEPTIONS**********\n" << std::endl;
+	try {
+		Form form1("form1", 10, 190);
+	} catch (std::exception &e) {
+		std::cout << "form1 -> Error: " << e.what() << std::endl;
+	}
+
+	try {
+		Form form2("form2", 10, 0);
+	} catch (std::exception &e) {
+		std::cout << "form2 -> Error: " << e.what() << std::endl;
+	}
+
+	try {
+		Form form3("form3", 190, 10);
+	} catch (std::exception &e) {
+		std::cout << "form3 -> Error: " << e.what() << std::endl;
+	}
+
+	try {
+		Form form4("form4", 0, 100);
+	} catch (std::exception &e) {
+		std::cout << "form4 -> Error: " << e.what() << std::endl;
+	}
+}
+
+static void testInvalidSing()
+{
+
+
+	std::cout << "\n***************** TESTING INVALID SING EXCEPTIONS**********\n" << std::endl;
+	try {
+		Bureaucrat bob("Bob", 10);
+		Form form("form1", 1, 120);
+		bob.signForm(form);
+	} catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+
+	std::cout << "\n*********TESTING INVALID SING EXCEPTIONS**********\n" << std::endl;
+	try {
+		Bureaucrat bob("Bob", 10);
+		Form form("form2", 10, 120);
+		bob.decreaseGrade();
+		bob.signForm(form);
+		bob.increaseGrade();
+		bob.increaseGrade();
+		bob.signForm(form);
+	} catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+
+}
+
+
 int main()
 {
-	Bureaucrat bob("Bob", 15);
-	Form f("Tax Evasion", 15, 150);
-	std::cout << f;
+	testConstructor();
 
-	bob.signForm(f);
-	bob.signForm(f);
-	// try
-	// {
-	// Form f("Tax Evasion", 15,150);
-	// std::cout << f << std::endl;
-	// }
-	// catch (std::exception &e)
-	// {
-	// 	std::cout << e.what();
-	// }
+	testInvalidSing();
+
+		std::cout << "\n*********TESTING CASE, THAT SIGN**********\n" << std::endl;
+	try {
+		Bureaucrat corrupt("Corrupt", 10);
+		Form form("Tax Evasion", 10,120);
+		corrupt.signForm(form);
+	} catch (std::exception &e){
+		std::cout << "Error: " << e.what() << std::endl;
+	}
 
 	return (0);
 }

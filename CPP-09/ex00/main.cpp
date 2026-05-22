567ae6425406cea57d1362df6f62dd82fe43fd8d
Jorge Marquez <jorgemiliano91@gmail.com>
2026-05-22 22:18:00 +0100

    Fix try and catch missing on CPP-09/ex00

--- CPP-09/ex00/main.cpp
+++ CPP-09/ex00/main.cpp
@@ -1,14 +1,18 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
 		std::cout << "Error: could not open file" << std::endl;
 		return (1);
 	}
-	BitcoinExchange btc;
-	btc.loadDatabase("data.csv");
-	btc.processInput(argv[1]);
+	try {
+		BitcoinExchange btc;
+		btc.loadDatabase("data.csv");
+		btc.processInput(argv[1]);
+	} catch (const std::exception &e) {
+		std::cout << e.what() << std::endl;
+		return (1);
+	}
+	return (0);
 }
